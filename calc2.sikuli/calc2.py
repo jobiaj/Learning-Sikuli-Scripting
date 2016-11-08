af6cbd5e67ebf6269157ddce9561b5a4cffbf451
jobiaj <jobyalungal@gmail.com>
2016-11-08 16:35:24 +0530

    First and second learn

---
+++ calc2.sikuli/calc2.py
@@ -0,0 +1,11 @@
+click("1478601478992.png")
+wait("1478601550253.png")
+type("calc"+ Key.ENTER)
+wait("1478602292947.png")
+click(Pattern("1478602292947.png").targetOffset(-79,20))
+click(Pattern("1478602292947.png").targetOffset(38,45))
+click(Pattern("1478602292947.png").targetOffset(-41,16))
+click(Pattern("1478602292947.png").targetOffset(80,32))
+
+
+
---
+++ first.sikuli/first.py
@@ -0,0 +1,12 @@
+click("1478601478992.png")
+wait("1478601550253.png")
+type("calc"+ Key.ENTER)
+wait("1478601789482.png")
+click("1478601803282.png")
+click("1478601811673.png")
+click("1478601819150.png")
+click("1478601829524.png")
+
+
+
+
